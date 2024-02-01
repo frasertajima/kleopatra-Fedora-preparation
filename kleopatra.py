f9bd3062653aaed473c65d2b30d1f23893230178
frasertajima <69366820+frasertajima@users.noreply.github.com>
2024-02-01 12:41:49 -0800

    Add files via upload

---
+++ kleopatra.py
@@ -0,0 +1,20 @@
+import subprocess
+
+# reset YubiKey to recognise card, enable decryption and encryption on command line so
+# Kleopatra can perform operations
+commands = [
+    'systemctl restart pcscd',
+    'gpg --card-status',
+    'gpg --output test.pdf --decrypt test.gpg',         # the file type will depend on 
+                                                        # what test file you encrypted
+    'echo "test" | gpg --clearsign'
+]
+
+for command in commands:
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print("Command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error {e.returncode}.")
+
+print("You should now be able to start up Kleopatra and encrypt/sign/decrypt/verify along with recognise the YubiKey as a smart-card.")
